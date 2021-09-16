9018c6b36ef1848371edf627022f605b1c0d936c
James Brady <jfbrady12345@gmail.com>
2021-09-17 10:56:39 +1200

    Set up complete.

--- Data/WebAPIQuiz335DBContext.cs
+++ Data/WebAPIQuiz335DBContext.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Quiz335.Data;
 
 namespace Quiz335.Data
 {
-    public class WebAPIQuiz335DBContext
+    public class WebAPIQuiz335DBContext : DbContext
     {
+        public WebAPIQuiz335DBContext(DbContextOptions<WebAPIQuiz335DBContext> options) : base(options) { }
+        public DbSet<>
+        public DbSet<>
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Sourve = <db name>.sqlite");
+        }
     }
 }
