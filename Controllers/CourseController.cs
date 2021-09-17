525d36aeaed4fa8cfb75b16731043d83b1931a7d
James Brady <jfbrady12345@gmail.com>
2021-09-17 12:13:57 +1200

    Completed endpoint 1.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Quiz335.Controllers
+{
+    [Route("quizapi")]
+    [ApiController]
+    public class CourseController : Controller
+    {
+        [HttpGet("GetCourseInfo/{code}")]
+        public ActionResult GetCourseInfo(string code)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string courseDir = Path.Combine(path, "Courses");
+            string fileName = Path.Combine(courseDir, code + ".html");
+            string respHeader = "";
+            if (System.IO.File.Exists(fileName))
+            {
+                respHeader = "text/html; charset=UTF-8";
+                return PhysicalFile(fileName, respHeader);
+            }
+            else
+            {
+                string responseString = "There is no course " + code;
+                return NotFound(responseString);
+            }
+        }
+    }
+}
---
+++ Controllers/MarksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quiz335.Controllers
+{
+    [Route("quizapi")]
+    [ApiController]
+    public class MarksController : Controller
+    {
+        [HttpGet("GetMarks")]
+        public async Task<ActionResult<IEnumerable<MarksOutDto>>> GetMarksAsync()
+        {
+            return View();
+        }
+    }
+}
